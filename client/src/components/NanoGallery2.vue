8b01b7ce9b6cb026b3c8f9e5fdb866cb7f6e627c
Alin CHIS <alin.d.chis@gmail.com>
2020-06-15 15:53:26 +0300

    adjusted nanogallery. freezes with many pictures

--- client/src/components/NanoGallery2.vue
+++ client/src/components/NanoGallery2.vue
@@ -1,46 +1,51 @@
 <template>
   <!-- ### position of the gallery ### -->
   <div
-    id="nanogallery2"
-    data-nanogallery2='{
+      id="nanogallery2"
+      data-nanogallery2='{
         "kind": "flickr",
         "thumbnailHeight": 150,
         "thumbnailWidth": 150,
       }'
   >
     <a
-      v-for="image in this.$store.state.monuments.selectedItem.images"
-      :key="image"
-      :href="image"
-      :data-ngthumb="image"
+        v-for="image in this.$store.state.monuments.selectedItem.images"
+        :key="image"
+        :href="image"
+        :data-ngthumb="image"
     >
     </a>
   </div>
 </template>
 
 <script>
-export default {
-  mounted() {
-    const $ = window.jQuery;
-    const fullPathImageArray = [...this.$store.state.monuments.selectedItem.images].map( imgPath => ({
-      src: imgPath,
-      srct: imgPath.replace('.jpg', '_thumb.jpg'),
-    }));
-    $('#nanogallery2').nanogallery2({
-      // ### gallery settings ###
-      thumbnailHeight: 150,
-      thumbnailWidth: 150,
-      // itemsBaseURL: '/',
+    export default {
+        mounted() {
+            const $ = window.jQuery;
+            const fullPathImageArray = [...this.$store.state.monuments.selectedItem.images].map(imgPath => ({
+                src: imgPath,
+                srct: imgPath.replace('.jpg', '_thumb.jpg'),
+            }));
+            $('#nanogallery2').nanogallery2({
+                // ### gallery settings ###
+                thumbnailHeight: 150,
+                thumbnailWidth: "auto",
+                thumbnailBorderHorizontal: 0,
+                thumbnailBorderVertical: 0,
+                thumbnailGutterWidth: 2,
+                thumbnailGutterHeight: 5,
+                thumbnailLabel:   { "display" : false },
+                // itemsBaseURL: '/',
 
-      // ### gallery content ###
-      items: fullPathImageArray,
-    });
-  },
+                // ### gallery content ###
+                items: fullPathImageArray,
+            });
+        },
 
-  data() {
-    return {};
-  },
-};
+        data() {
+            return {};
+        },
+    };
 </script>
 
 <style scoped></style>
--- client/src/components/PicturesGallery.vue
+++ client/src/components/PicturesGallery.vue
@@ -7,63 +7,18 @@
         transition-show="slide-up"
         transition-hide="slide-down"
     >
-    <nano-gallery-2 />
-      <!-- <q-card class="bg-grey-9 text-white">
+      <q-card class="bg-grey-9 text-white">
 
         <q-card-section class="q-pa-md row items-start q-gutter-md" v-if="$q.platform.is.desktop">
           <q-space/>
+          <h5>{{this.$store.state.monuments.selectedItem ? this.$store.state.monuments.selectedItem['cod_lmi'] : ''}}: {{this.$store.state.monuments.selectedItem ? this.$store.state.monuments.selectedItem['denumire'] : ''}}</h5>
+          <q-space/>
+          <q-btn type="button" @click="dialog = false">Close</q-btn>
         </q-card-section>
 
-        <q-card-section class="q-pa-md row items-start q-gutter-md" v-if="this.$store.state.monuments.selectedItem && !carousel">
+      <nano-gallery-2/>
 
-          <q-card
-              class="my-card"
-              v-for="(image, index) in this.$store.state.monuments.selectedItem.images"
-              :key="index"
-          >
-            <q-img
-                :src="image"
-                basic
-                :ratio="1"
-                @click="slide = index; carousel = true;"
-            ></q-img>
-          </q-card>
-
-          <q-page-sticky z-max position="top-right" :offset="[15, -30]" v-if="$q.platform.is.mobile">
-            <q-btn fab color="red" icon="clear" type="button" v-close-popup/>
-          </q-page-sticky>
-
-          <q-page-sticky z-max position="top-right" :offset="[-350, -80]" v-if="$q.platform.is.desktop">
-            <q-btn fab color="red" icon="clear" type="button" v-close-popup/>
-          </q-page-sticky>
-        </q-card-section>
-
-        <q-card-section class="q-pa-none fit" v-if="this.$store.state.monuments.selectedItem && carousel">
-          <q-carousel
-              animated
-              v-model="slide"
-              arrows
-              navigation
-              infinite
-              dark
-              thumbnails
-          >
-            <q-carousel-slide
-                v-for="(image, index) in this.$store.state.monuments.selectedItem.images"
-                :key="index"
-                :name="index"
-                :img-src="image"
-                dark
-            />
-          </q-carousel>
-          <q-page-sticky z-max position="top-right" :offset="[15, -30]" v-if="$q.platform.is.mobile">
-            <q-btn fab color="grey-9" icon="clear" type="button" @click="carousel = false"/>
-          </q-page-sticky>
-        </q-card-section>
-
-
-
-      </q-card> &ndash;&gt;-->
+      </q-card>
     </q-dialog>
   </div>
 </template>
